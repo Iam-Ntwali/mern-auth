1743b72127a8c4d364b7693e7159f95be92868d0
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-08-20 16:16:12 +0200

    Some changes 1

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();  // Load environment variables from a .env file into process.env
 
 const app = express();
 
@@ -6,6 +11,9 @@ app.get('/', (req, res) => {
   res.send('Hello Nodemond! 🙂');
 });
 
+app.use('/api/auth', authRoutes);
+
 app.listen(3000, () => {
+  connectDB();
   console.log('Server is running on http://localhost:3000');
 });
